841f974f90a16081f4857d3751613712a4fba5e1
Matsuda <matsu20010403@gmail.com>
2023-11-11 14:47:24 +0900

    add train.py

--- train.py
+++ train.py
@@ -1,50 +1,17 @@
-# 概要
-# 歌詞からアーティスト名を予測する分類モデルを学習させるコードです。
-# テキストを文字単位で分割して入力する「CharacterCNN」を採用しています。
-# 論文→https://proceedings.neurips.cc/paper_files/paper/2015/file/250cf8b51c773f3f8dc8b4be867a9a02-Paper.pdf
-
-# ライブラリのインポート
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
-import japanize_matplotlib
-from tqdm import tqdm
 import copy
 import os
-import json
-import datetime
+from collections import OrderedDict
 
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from sklearn.model_selection import train_test_split, GroupKFold
+from sklearn.model_selection import GroupKFold
+from sklearn.metrics import f1_score, accuracy_score
 
 # 保存先のパス
-PATH = './'
-
-# 歌詞CSVの読み込み
-df = pd.read_csv(PATH+'data/lyric_block.csv')
-print(df.artist.unique())
-
-# アーティストを選択
-# - `artists`に分類先のアーティスト名を格納する。
-
-# 分類対象のアーティストを選択
-artists = ['あいみょん', 'スピッツ', '星野源', 'YOASOBI']
-df_sub = df[df['artist'].isin(artists)]
-
-# アーティスト名を整数化(idとは別。ラベル用)
-artist_to_label = {artist:i for i,artist in enumerate(artists)}
-
-# 各アーティストのデータ数を棒グラフで可視化
-plt.figure(figsize=(10,3))
-df_sub['artist'].value_counts().plot(kind='bar')
-plt.show()
-
-
-# 前処理
-# - 各文字をUnicodeに変換
-# - 特徴量をXへ、ラベルをyへ格納
+PATH = '.'
 
 def encode(txt, max_length=200):
     """
@@ -78,20 +45,10 @@ def encode(txt, max_length=200):
         txt_list.append((txt_line))
     return np.array(txt_list)
 
-
-# 説明変数X
-X = encode(df_sub['block'])
-# 目的変数y
-y = df_sub['artist'].map(artist_to_label).values
-print(X.shape, y.shape)
-
-# ネットワークの定義
 class CharacterCNN(nn.Module):
-    def __init__(self, num_classes ,embed_size=128, max_length=200, filter_sizes=(2, 3, 4, 5), filter_num=64):
+    def __init__(self, num_classes, embed_size=256, filter_sizes=(2, 3, 4, 5), filter_num=64):
         super().__init__()
-        self.params = {'num_classes': num_classes ,'embed_size':embed_size, 'max_length':max_length, 'filter_sizes':filter_sizes, 'filter_num':filter_num}
         self.embed_size = embed_size
-        self.max_length = max_length
         self.filter_sizes = filter_sizes
         self.filter_num = filter_num
 
@@ -120,13 +77,162 @@ class CharacterCNN(nn.Module):
         fc2_output = self.fc2(do_output)
         return fc2_output
 
-# 学習
-# - `n_epochs`, `batch_size`, `learning_rate`, `optimizer`などを設定する。
-# - 同じ歌の歌詞が、学習用データと評価用データへ混在しないようにGroupKFold。（1曲の中で同じ歌詞が繰り返し登場するため）
-# - 各foldのBestモデルを保存する。
+class Trainer:
+    def __init__(self, n_epochs, batch_size, learning_rate, criterion, gkf, groups, pretrain, device):
+        self.batch_size = batch_size
+        self.device = device
+        self.learning_rate = learning_rate
+        self.n_epochs = n_epochs
+        self.criterion = criterion
+        self.gkf = gkf
+        self.groups = groups
+        self.pretrain = pretrain
+        self.n_splits = gkf.get_n_splits()
+        self.bst_model, self.bst_score = dict(), dict()
 
-n_epochs = 10          # エポック数
-batch_size = 256       # バッチサイズ
+    def set_model(self, network, state_dict):
+        self.network = network
+        self.state_dict = copy.deepcopy(state_dict)
+
+    def reset_model(self):
+        model = self.network
+        model.load_state_dict(self.state_dict)
+        return model
+
+    def train(self, X, y, verbose=1):
+        for fold, (tr_idx, va_idx) in enumerate(self.gkf.split(X, y, groups=self.groups)):
+            # 学習データと評価用データに分割
+            print(f'-----Fold{fold+1}/{self.n_splits}-----')
+            X_tr, X_va = X[tr_idx], X[va_idx]
+            y_tr, y_va = y[tr_idx], y[va_idx]
+            n_iter = len(y_tr) // self.batch_size
+
+            model = self.reset_model().to(self.device)
+            optimizer = torch.optim.Adam(model.parameters(), lr=self.learning_rate)
+
+            self.bst_model[fold] = None
+            self.bst_score[fold] = -np.inf
+
+            # 学習
+            for epoch in range(self.n_epochs):
+                model.train()
+                total_loss = 0.0
+                total_correct = 0
+                random_idx = np.random.permutation(len(y_tr))
+                for i in range(n_iter):
+                    X_batch = torch.from_numpy(X_tr[random_idx[self.batch_size*i:self.batch_size*(i+1)]]).to(self.device)
+                    y_batch = torch.from_numpy(y_tr[random_idx[self.batch_size*i:self.batch_size*(i+1)]]).to(self.device)
+
+                    optimizer.zero_grad()
+                    outputs = model(X_batch)
+                    loss = self.criterion(outputs, y_batch)
+                    loss.backward()
+                    optimizer.step()
+
+                    total_loss += loss.item()
+                    _, predicted = outputs.max(dim=1)
+                    total_correct += (predicted == y_batch).sum().item()
+
+                train_loss = total_loss / n_iter
+                train_acc = total_correct / (self.batch_size*n_iter)
+
+                # 評価
+                model.eval()
+                with torch.no_grad():
+                    outputs = model(torch.from_numpy(X_va).to(self.device))
+                    loss = criterion(outputs, torch.from_numpy(y_va).to(self.device))
+                    valid_loss = loss.item()
+                    _, predicted = outputs.max(dim=1)
+                    valid_acc = accuracy_score(y_va, predicted.cpu().numpy())
+                    valid_f1 = f1_score(y_va, predicted.cpu().numpy(), average='macro')
+                if (epoch+1) % verbose == 0:
+                    print(f'Epoch[{epoch+1}/{self.n_epochs}], TrainLoss: {train_loss:.4f}, ValidLoss: {valid_loss:.4f}, TrainAcc: {train_acc*100:.4f}%, ValidAcc: {valid_acc*100:.4f}%, ValidF1: {valid_f1:.4f}')
+
+                # best更新処理
+                if valid_f1 > self.bst_score[fold]:
+                    self.bst_model[fold] = copy.deepcopy(model)
+                    self.bst_score[fold] = valid_f1
+
+            if self.pretrain:
+                break
+
+    def save_all(self, dirname):
+        os.makedirs(dirname, exist_ok=True)
+        for fold, model in self.bst_model.items():
+            filepath = dirname + '/' + f'model_fold{fold+1}.pth'
+            torch.save(model.state_dict(), filepath)
+            print(f'[Saved] score:{self.bst_score[fold]:.4f}  @ {filepath}')
+
+    def save_clf(self, dirname):
+        os.makedirs(dirname, exist_ok=True)
+        for fold, model in self.bst_model.items():
+            filepath = dirname + '/' + f'classifier_fold{fold+1}.pth'
+            clf_s = OrderedDict(list(model.state_dict().items())[1:])
+            torch.save(clf_s, filepath)
+            print(f'[Saved] score:{self.bst_score[fold]:.4f}  @ {filepath}')
+
+# 歌詞CSVの読み込み
+df = pd.read_csv(PATH + '/data/lyric_block.csv')
+print(df.artist.unique())
+
+# 分類先アーティスト
+artists = list(df.artist.unique())
+artist_to_label = {artist:i for i,artist in enumerate(artists)}
+
+# 説明変数X
+X = encode(df['block'])
+# 目的変数y
+y = df['artist'].map(artist_to_label).values
+
+
+n_epochs = 20          # エポック数
+batch_size = 512       # バッチサイズ
+learning_rate = 0.001  # 学習率
+n_splits = 5           # GroupKFoldの分割数
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+print(device)
+
+criterion = nn.CrossEntropyLoss()
+gkf = GroupKFold(n_splits,)  # GroupKfold
+
+def seed_everything(seed: int):
+    import random, os
+    import numpy as np
+    import torch
+
+    random.seed(seed)
+    os.environ['PYTHONHASHSEED'] = str(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = True
+
+seed_everything(0)
+model = CharacterCNN(num_classes=len(artists), embed_size=256, filter_sizes=(2,3,4,5), filter_num=64)
+
+pretrain = Trainer(n_epochs, batch_size, learning_rate, criterion, gkf, df['title'], True, device)
+pretrain.set_model(model, model.state_dict())
+pretrain.train(X, y)
+
+pretrain.save_all(PATH + '/pretrain')
+
+
+# 事前学習重みを読み込む
+weight_path = PATH + '/pretrain/model_fold1.pth'
+model = CharacterCNN(num_classes=len(artists), embed_size=256, filter_sizes=(2,3,4,5), filter_num=64)
+model.load_state_dict(torch.load(weight_path))
+
+# Embeddingのみweightを固定
+for param in model.parameters():
+    param.requires_grad = False
+    break
+
+# 出力層を変更
+model.fc2 = nn.Linear(64, 2, bias=True)
+
+n_epochs = 20          # エポック数
+batch_size = 128       # バッチサイズ
 learning_rate = 0.001  # 学習率
 n_splits = 5           # GroupKFoldの分割数
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
@@ -135,81 +241,15 @@ print(device)
 criterion = nn.CrossEntropyLoss()
 gkf = GroupKFold(n_splits)  # GroupKfold
 
-now = datetime.datetime.now().strftime('%Y%m%d%H%M%S%f')
-for fold, (tr_idx, va_idx) in enumerate(gkf.split(X, y, groups=df_sub['title'])):
-    # 学習データと評価用データに分割
-    print(f'-----Fold{fold+1}/{n_splits}-----')
-    X_tr, X_va = X[tr_idx], X[va_idx]
-    y_tr, y_va = y[tr_idx], y[va_idx]
-    n_iter = len(y_tr)//batch_size
+for artist in artists:
+    df_artist = df[df['artist'] == artist]
+    df_other = df[df['artist'] != artist].sample(len(df_artist))
+    df_sub = pd.concat([df_artist, df_other])
 
-    bst_model = None
-    bst_score = np.inf
-    model = CharacterCNN(num_classes=len(artists), embed_size=128, filter_sizes=(2,3,4,5), filter_num=64).to(device)
-    optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+    X = encode(df_sub['block'])
+    y = df_sub['artist'].map({artist:1}).fillna(0).values.astype('int')
 
-    # 学習
-    for epoch in range(n_epochs):
-        model.train()
-        total_loss = 0.0
-        total_correct = 0
-        random_idx = np.random.permutation(len(y_tr))
-        for i in range(n_iter):
-            X_batch = torch.from_numpy(X_tr[random_idx[batch_size*i:batch_size*(i+1)]]).to(device)
-            y_batch = torch.from_numpy(y_tr[random_idx[batch_size*i:batch_size*(i+1)]]).to(device)
-
-            optimizer.zero_grad()
-            outputs = model(X_batch)
-            loss = criterion(outputs, y_batch)
-            loss.backward()
-            optimizer.step()
-
-            total_loss += loss.item()
-            _, predicted = outputs.max(dim=1)
-            total_correct += (predicted == y_batch).sum().item()
-
-        train_loss = total_loss / n_iter
-        train_acc = total_correct / (batch_size*n_iter)
-
-        # 評価
-        model.eval()
-        with torch.no_grad():
-            outputs = model(torch.from_numpy(X_va).to(device))
-            loss = criterion(outputs, torch.from_numpy(y_va).to(device))
-            valid_loss = loss.item()
-            _, predicted = outputs.max(dim=1)
-            valid_acc = (predicted == torch.from_numpy(y_va).to(device)).sum().item() / len(y_va)
-        print(f'Epoch[{epoch+1}/{n_epochs}], TrainLoss: {train_loss:.4f}, ValidLoss: {valid_loss:.4f}, TrainAcc: {train_acc*100:.4f}%, ValidAcc: {valid_acc*100:.4f}%')
-
-        # best更新処理
-        if valid_loss < bst_score:
-            bst_model = copy.deepcopy(model)
-            bst_score = valid_loss
-            bst_epoch = epoch + 1
-
-    # ベストモデルを保存
-    os.makedirs(PATH+f'models/model_{now}', exist_ok=True)
-    torch.save(bst_model.cpu(), PATH+f'models/model_{now}/model_fold{fold+1}.pth')
-
-    # モデルの情報をjsonに記録
-    if fold == 0:
-        data = dict()
-        data['ClassNames'] = artists
-
-    bst_info = dict()
-    bst_info['TrainSize'] = len(y_tr)
-    bst_info['ValidSize'] = len(y_va)
-    bst_info['Params'] = model.params
-    bst_info['BatchSize'] = batch_size
-    bst_info['LearningRate'] = learning_rate
-    bst_info['Optimizer'] = str(type(optimizer))
-    bst_info['Epoch'] = bst_epoch
-    bst_info['TrainLoss'] = train_loss
-    bst_info['ValidLoss'] = valid_loss
-    bst_info['TrainAcc'] = train_acc * 100
-    bst_info['ValidAcc'] = valid_acc * 100
-
-    data[f'Fold{fold+1}'] = bst_info
-
-with open(PATH + f'models/model_{now}/info.json', 'w') as f:
-    json.dump(data, f)
+    train = Trainer(n_epochs, batch_size, learning_rate, criterion, gkf, df_sub['title'], False, device)
+    train.set_model(model, model.state_dict())
+    train.train(X, y, 10)
+    train.save_clf(PATH + f'/models/{artist}')
